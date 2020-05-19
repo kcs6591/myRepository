b6c891a33d4f2ff43d7ebe53690f1cab1cf9f7b5
kcs65 <kcs65@LAPTOP-10VR9M1Q>
2020-05-19 10:39:18 +0900

    // branch commit 1

--- mask_test/src/main/java/com/spring/mask/MapController.java
+++ mask_test/src/main/java/com/spring/mask/MapController.java
@@ -36,6 +36,8 @@ public class MapController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	// 들어오 주소기준 map.jsp 호출	
 	@RequestMapping(value = "/map", method = RequestMethod.GET)
 	public String Map(@RequestParam("addr") String search, Model model) 
 	{
